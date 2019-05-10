7b3def43820eab042cad52b586c2ed9ecb1fb309
dominik weber <dweber@lsv.uni-saarland.de>
2019-05-10 08:22:09 +0200

    fixes

--- exercises/Ex2/prob_dist.py
+++ exercises/Ex2/prob_dist.py
@@ -27,11 +27,12 @@ def prob_dist(N,S,h):
 		prob[c] = N[h+c]/sumNS      #calculates the prob for c
 
 	return prob
+
 if __name__ == '__main__':
-	lan = 'de' #or .en
+	lan = 'de' # de or en
 	path = './corpora/' #path to the corpora folder
 
-	h = 'n'
+	h = 'ung'
 	S = list(string.ascii_lowercase)
 	if lan == 'de':
 		S = S + ['ä','ö','ü','ß']
@@ -41,13 +42,12 @@ if __name__ == '__main__':
 	handler = fa.CorporaHandler(corpus)
 	handler.corpus_to_ngrams(len(h)+1)
 	prob_dist = prob_dist(handler.ngrams,S,h)
-	print(round(sum(prob_dist.values()),7))
 	assert round(sum(prob_dist.values()),7)==1 
 
 	E = entropy(prob_dist, S)
 	print(E)
 
-if False:    #turn to true if you want plot the distribution
+if True:    #turn to true if you want plot the distribution
 	plt.figure(num=1, figsize=(1,len(S)), dpi=None, facecolor=None, edgecolor=None, frameon=True, clear=False)
 	plt.plot(scalex=True, scaley=True, data=None)
 	plt.bar(prob_dist.keys(),prob_dist.values())
